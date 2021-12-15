d7675de2d606a03622dfee7b4d9223a41e10be61
Thiraphatrom <42799125+thiraphatrom@users.noreply.github.com>
2021-12-15 11:51:37 +0700

    Add files via upload

---
+++ 15-12-2021/App_V1.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Text } from "react-native";
+// import TextInputReview from './components/TextInputReview';
+// import ImageWithTextInput from './components/ImageWithTextInput';
+// import ImageWithTextInput from './components/ButtonExample';
+// import ButtonExample from "./components/ButtonExample";
+// import TextInputPractice1 from './components/TextInputPractice1';
+// import TouchableExample from './components/TouchableExample';
+// import TouchablePractice from './components/TouchablePractice';
+import Logo from "./components/Logo";
+
+const App = () => {
+  return (
+    // <TextInputReview/>
+    // <ImageWithTextInput/>
+    // <ButtonExample/>
+    // <TextInputPractice1/>
+    // <TouchableExample/>
+    // <TouchablePractice/>
+    <View>
+      <Logo/>
+      <Text>Hello React Native</Text>
+      <Text>สวัสดี React Native</Text>
+    </View>
+  );
+}
+
+export default App;
